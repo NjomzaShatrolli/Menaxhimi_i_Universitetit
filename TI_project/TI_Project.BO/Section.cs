f5631fbaa8aae6f6a4e88e53d44d0c21d52a6f10
NjomzaShatrolli <107035580+NjomzaShatrolli@users.noreply.github.com>
2023-04-03 22:07:09 -1200

    Update Section.cs

--- TI_project/TI_Project.BO/Section.cs
+++ TI_project/TI_Project.BO/Section.cs
@@ -12,11 +12,8 @@ namespace TI_Project.BO
         public int Year { get; set; }
         public DateTime Start_Time { get; set; }
         public DateTime End_Time { get; set;}
-
         public string Room_Number { get; set; }
-
         public Course Course_Id { get; set; }
-
         public Faculty Faculty_Id { get; set; }
     }
 }
