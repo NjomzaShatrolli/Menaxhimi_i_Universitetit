b664edf28a1665de0361053a120a63e7df55ea9b
NjomzaShatrolli <107035580+NjomzaShatrolli@users.noreply.github.com>
2023-04-03 22:09:20 -1200

    Update Enrollment.cs

--- TI_project/TI_Project.BO/Enrollment.cs
+++ TI_project/TI_Project.BO/Enrollment.cs
@@ -9,11 +9,11 @@ namespace TI_Project.BO
     public class Enrollment : Base
     {
         public DateTime Enrollment_Date { get; set; }
-
+      
         public string Grade { get; set; }
-
+        
         public Section Section_Id { get; set; }
-
+        
         public Student Student_Id { get; set; }
     }
 }
